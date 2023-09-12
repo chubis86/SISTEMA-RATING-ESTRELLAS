af42ee15cb841a47af6937b324de80ef97face96
EFREN NARVAEZ <efrennm86@gmail.com>
2023-09-12 14:50:16 -0600

    Solucionado el problema de la estrella inicial

--- main.js
+++ main.js
@@ -9,11 +9,25 @@ for(let x=0; x<limit; x++){
 
 rating.innerHTML = html;
 
-document.querySelectorAll('.item').forEach(item=>{
-    item.addEventListener('mouseover', e =>{
-       let elemento = item.getAttribute('data-pos');
+document.querySelectorAll('.item').forEach(item =>{
+    
+    let elemento = item.getAttribute('data-pos');
+
+    //Al salir
+    item.addEventListener('mouseout', e=>{
+        document.querySelectorAll('.item').forEach(it=>{
+
+            it.classList.remove("item-full");    
+            
+       });
+    });
+    
+
+    //Al entrar
+    item.addEventListener('mouseover', e=>{
        
-       document.querySelectorAll('.item').forEach(item2=>{
+        
+        document.querySelectorAll('.item').forEach(item2=>{
 
             if(item2.getAttribute('data-pos')<=elemento){
                 if(item2.classList.contains("item-full")){
@@ -24,19 +38,9 @@ document.querySelectorAll('.item').forEach(item=>{
             }else{
                 item2.classList.remove("item-full");
             }
-       });
-    
-       currentValue=parseInt(elemento)+1;
-       
+       });     
     });
 
     
 });
 
-   
-/* document.querySelectorAll('.item').forEach(item3=>{
-    item3.addEventListener('onmouseout', e=>{
-        item3.classList.remove("item-full"); 
-    }); 
-});
- */
